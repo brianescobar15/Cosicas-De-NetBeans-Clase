91805e81a45ff291bc98630824504958e0c0891c
brianescobar15 <brianer81@educastur.es>
2020-11-10 15:15:25 +0100

    Primera subida

---
+++ Atajos/src/dto/Coche.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dto;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author dams1
+ */
+public class Coche {
+
+    private String matricula, modelo;
+
+    public Coche(String matricula, String modelo) {
+        this.matricula = matricula;
+        this.modelo = modelo;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    @Override
+    public String toString() {
+        return "coche{" + "matricula=" + matricula + ", modelo=" + modelo + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + Objects.hashCode(this.matricula);
+        hash = 37 * hash + Objects.hashCode(this.modelo);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Coche other = (Coche) obj;
+        if (!Objects.equals(this.matricula, other.matricula)) {
+            return false;
+        }
+        if (!Objects.equals(this.modelo, other.modelo)) {
+            return false;
+        }
+        return true;
+    }
+
+}
